e4a5d0152c8e812291954d628f7c776a6c87baaa
lndsld <ramiro.cruz@mail.ru>
2018-10-17 22:40:04 +0300

    More animations

--- script.js
+++ script.js
@@ -8,18 +8,18 @@ let slides = [
 		text: "Вы получите концепцию прибыльной автоворонки для Вашей ниши бизнеса с внедрением под ключ. Создадим цепочки писем, SMS сообщений, автозвонков в зависимости от поведения покупателей на сайте и интегрируем всё это в CRM.</p><p>Возвратите ушедших посетителей, повысите средний чек и чистую прибыль с каждого заказа на сайте."
 	},
 	{
-		name: "Design",
-		img: "assets/slider/label.png",
-		color: "#45efb5",
-		header: "Увеличьте поток заказов с сайта на 20-30% через Яндекс Директ и Google Adwords",
-		text: "Сделаем анализ текущих рекламных кампаний и подготовим план работ по улучшению показателей. После этого создадим новый аккаунт с учётом всех правок.</p><p>Вам останется пополнить рекламный бюджет и сравнить показатели \"было/стало\"."
-	},
-	{
 		name: "Code",
 		img: "assets/slider/tube.png",
 		color: "#ffc000",
 		header: "Получайте целевых посетителей на Ваш сайт и сообщество в социальных сетях",
 		text: "Таргетированная реклама в Vk, Facebook и Instagram. Показывайте рекламу только целевым клиентам, которые уже готовы купить Ваш товар.</p><p>Создадим стратегию продвижения в социальных сетях, которая удвоит поток лояльных клиентов в Ваш бизнес."
+	},
+	{
+		name: "Design",
+		img: "assets/slider/label.png",
+		color: "#45efb5",
+		header: "Увеличьте поток заказов с сайта на 20-30% через Яндекс Директ и Google Adwords",
+		text: "Сделаем анализ текущих рекламных кампаний и подготовим план работ по улучшению показателей. После этого создадим новый аккаунт с учётом всех правок.</p><p>Вам останется пополнить рекламный бюджет и сравнить показатели \"было/стало\"."
 	}
 ]
 
@@ -57,10 +57,10 @@ function Slider() {
 	let lines = document.getElementsByClassName("line")
 
 	// Initial state
-	let currentNum = 0
-	image.src = slides[0].img
-	text.innerHTML = slides[0].text
-	header.innerHTML = slides[0].header
+	let currentNum = -1
+	// image.src = slides[0].img
+	// text.innerHTML = slides[0].text
+	// header.innerHTML = slides[0].header
 
 	// Slide setter
 	this.set = newNum => {
@@ -88,8 +88,6 @@ function Slider() {
 			image.src = slides[newNum].img
 			text.innerHTML = slides[newNum].text
 			header.innerHTML = slides[newNum].header
-			//textBox.classList.remove("hidden")
-
 		}, 350)
 
 		// Remove animation classes
@@ -168,4 +166,6 @@ function Slider() {
 	rightButton.onclick = _ => {
 		this.turnRight()
 	}
+
+	this.set(0)
 }
